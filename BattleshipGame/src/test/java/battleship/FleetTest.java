8a2fecace75145b9e538805a0906c4e5f7ff4d42
britoeabreu <fernando.brito.abreu@gmail.com>
2023-03-18 13:01:29 +0000

    Maven configuration

---
+++ BattleshipGame/src/test/java/battleship/FleetTest.java
@@ -0,0 +1,107 @@
+/**
+ * 
+ */
+package battleship;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author fba
+ *
+ */
+class FleetTest
+{
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception
+    {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @AfterAll
+    static void tearDownAfterClass() throws Exception
+    {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeEach
+    void setUp() throws Exception
+    {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @AfterEach
+    void tearDown() throws Exception
+    {
+    }
+
+    /**
+     * Test method for {@link battleship.Fleet#Fleet()}.
+     */
+    @Test
+    final void testFleet()
+    {
+	fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link battleship.Fleet#addShip(battleship.IShip)}.
+     */
+    @Test
+    final void testAddShip()
+    {
+	fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link battleship.Fleet#listShipsLike(java.lang.String)}.
+     */
+    @Test
+    final void testListShipsLike()
+    {
+	fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link battleship.Fleet#listFloatingShips()}.
+     */
+    @Test
+    final void testListFloatingShips()
+    {
+	fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link battleship.Fleet#listAllShips()}.
+     */
+    @Test
+    final void testListAllShips()
+    {
+	fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link battleship.Fleet#shipAt(battleship.IPosition)}.
+     */
+    @Test
+    final void testShipAt()
+    {
+	fail("Not yet implemented"); // TODO
+    }
+
+}
